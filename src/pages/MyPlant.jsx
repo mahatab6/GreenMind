a8f6a72bd1a984b714a3c52b966cb6bb6eebcabb
mahatab6 <mahatabcall56@gmail.com>
2025-05-21 09:45:41 +0600

    single data find out done

--- src/pages/MyPlant.jsx
+++ src/pages/MyPlant.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const MyPlant = () => {
+    const {activeUser} = useContext(AuthContext);
+
+    const email = activeUser?.email ;
+
+    const [allPlant, setAllPlant] = useState([]);
+  
+    useEffect(()=>{
+        fetch('http://localhost:3000/all-plants')
+        .then(res => res.json())
+        .then(data => {
+            setAllPlant(data)
+          })
+      },[])
+    
+
+      const myPlantData = allPlant.filter((plant)=> plant.email === email);
+
+      
+   
+
     return (
         <div>
-            
+           
         </div>
     );
 };
