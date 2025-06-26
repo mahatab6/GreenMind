d023f644c611f4940e8cdd4df7fe4af74d1534e0
mahatab6 <mahatabcall56@gmail.com>
2025-06-26 15:03:45 +0600

    latest plant added

--- src/homeLayout/HomeComponents/NewPlant.jsx
+++ src/homeLayout/HomeComponents/NewPlant.jsx
@@ -15,44 +15,53 @@ const NewPlant = () => {
   }, []);
 
   return (
-    
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 justify-items-center py-10">
-      {display.map((treePlant) => (
-        <div
-          key={treePlant._id}
-          className="max-w-lg p-6 rounded-2xl shadow-md bg-white"
-        >
-          <div className="space-y-4">
-            <img
-              src={treePlant.image}
-              alt=''
-              className="block object-cover object-center w-full rounded-md h-72"
-            />
-            <div className="space-y-2">
-              <h3 className="text-2xl font-semibold text-green-700">
-                Plant name: {treePlant.plantName}
-              </h3>
-              <div className=" items-center justify-between">
-                <p className="text-xl font-normal text-black">
-                  Category: {treePlant.category}
-                </p>
-                <p className="text-xl font-normal text-black">
-                  Care Level: {treePlant.careLevel}
-                </p>
-                <p className="text-xl font-normal text-black">
-                  Watering frequency: {treePlant.wateringFrequency}
-                </p>
+
+    <div className='py-10 '>
+      <div className='text-center'>
+        <h2 className='text-4xl font-bold'>Latest Plant Additions</h2>
+        <p className='text-xl py-2'>Discover what fellow plant lovers are growing</p>
+      </div>
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center py-10">
+        {display.map((treePlant) => (
+          <div
+            key={treePlant._id}
+            className="max-w-lg p-6 rounded-2xl shadow-md bg-white hover:shadow-2xl"
+          >
+            <div className="space-y-4">
+              <img
+                src={treePlant.image}
+                alt=''
+                className="block object-cover object-center w-full rounded-md h-72"
+              />
+              <div className="space-y-2">
+                <h3 className="text-2xl font-semibold text-green-700">
+                  Plant name: {treePlant.plantName}
+                </h3>
+                <div className=" items-center justify-between">
+                  <p className="text-xl font-normal text-black">
+                    Category: {treePlant.category}
+                  </p>
+                  <p className="text-xl font-normal text-black">
+                    Care Level: {treePlant.careLevel}
+                  </p>
+                  <p className="text-xl font-normal text-black">
+                    Watering frequency: {treePlant.wateringFrequency}
+                  </p>
+                </div>
               </div>
+              <Link
+                to={`/plants-details/${treePlant._id}`}
+                className="btn w-full bg-green-500 shadow-md border-0  text-white hover:bg-green-600"
+              >
+                View Details
+              </Link>
             </div>
-            <Link
-              to={`/plants-details/${treePlant._id}`}
-              className="btn w-full bg-green-500 shadow-md border-0  text-white hover:bg-green-600"
-            >
-              View Details
-            </Link>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className=' text-center'>
+        <Link to="/all-plants" className='btn w-40 bg-green-500 hover:bg-green-600 text-white border-0 shadow-md'>All Plants</Link>
+      </div>
     </div>
   );
 };
